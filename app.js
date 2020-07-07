a1797db0b7ffe25eba6d5728d04552888bdc111d
varvara <varvaralipunova@gmail.com>
2020-07-07 04:32:59 +0000

    initial deploy commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require("express");
+	 app = express(),
+	 bodyParser = require("body-parser"),
+	 mongoose = require("mongoose"),
+	 flash = require("connect-flash"),
+	 passport = require("passport"),
+	 LocalStrategy = require("passport-local"),
+	 methodOverride = require("method-override"),
+	 Campground = require("./models/campground"),
+	 Comment = require("./models/comment"),
+	 User = require("./models/user"),
+     seedDB = require("./seeds")
+
+// requring routes
+var commentRoutes = require("./routes/comments"),
+	campgroundRoutes = require("./routes/campgrounds"),
+	authRoutes = require("./routes/auth")
+
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+// seed the database
+// seedDB();
+
+// Passport Configuration
+app.use(require("express-session")({
+	secret: "Once again Funtik is the best cat in the world",
+	resave :false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+});
+
+app.use("/", authRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/campgrounds", campgroundRoutes);
+
+app.listen(3000, function() { 
+  console.log('Server is listening to port 3000'); 
+});
+
---
+++ routes/campgrounds.js
@@ -0,0 +1,88 @@
+var express = require("express");
+var router = express.Router();
+var Campground = require("../models/campground");
+var middleware = require("../middleware");
+
+// INDEX - show all campgrounds
+router.get("/", function(req, res){
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		} else {
+			 res.render("campgrounds/index", {campgrounds:allCampgrounds, currentUser: req.user});
+		}
+	});
+});
+
+// CREATE - add a new campground to DB
+router.post("/", middleware.isLoggedIn, function(req, res){
+	var name = req.body.name;
+	var price = req.body.price;
+	var image = req.body.image;
+	var description = req.body.description;
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	}
+	var newCampground = {name: name, price: price, image: image, description: description, author: author}
+// 	Create a new campground and save to DB
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			console.log(newlyCreated);
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+
+// NEW - show form to create a new campground
+router.get("/new", middleware.isLoggedIn, function(req, res){
+	res.render("campgrounds/new");
+});
+
+// SHOW - shows more info about one campground
+router.get("/:id", function(req, res){
+// 	find the campgound with provided ID
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("campgrounds/show", {campground: foundCampground});
+		}
+	});
+});
+
+// EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", middleware.checkCampgroundOwnership, function(req, res){
+	Campground.findById(req.params.id, function(err, foundCampground){
+		res.render("campgrounds/edit", {campground: foundCampground});
+	});
+});
+
+// UPDATE CAMPGROUND ROUTE
+router.put("/:id", middleware.checkCampgroundOwnership, function(req, res){
+// 	find and update the correct campgrounds	
+	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+		if(err){
+			res.redirect("/campgrounds");
+		} else {
+			// 	redirect somewhere(show page)
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
+// DESTROY Campground route
+router.delete("/:id", middleware.checkCampgroundOwnership, function(req, res){
+	Campground.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/campgrounds");
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+module.exports = router;
